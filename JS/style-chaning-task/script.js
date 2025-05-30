0ddcb8a4077f3f3bc8379a51e1d891132ad87641
Rushyendra Guntupalli <74148058+dog-broad@users.noreply.github.com>
2025-05-30 12:28:45 +0530

    Add employee registration form with style selection and dynamic styling

---
+++ JS/style-chaning-task/script.js
@@ -0,0 +1,9 @@
+function changeStyle(n) {
+    if (n === 1) {
+        document.getElementById('styleSheet').href = 'style1.css';
+    } else if (n === 2) {
+        document.getElementById('styleSheet').href = 'style2.css';
+    } else if (n === 3) {
+        document.getElementById('styleSheet').href = 'style3.css';
+    }
+}
